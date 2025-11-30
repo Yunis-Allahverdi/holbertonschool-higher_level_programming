606205e9d5c252b776d26518cdff3a30805d1b4d
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-11-30 14:45:38 +0000

    Updated

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     symbol = "{}".format(chr(i))
-    if(symbol == 'q' or symbol == 'e'):
-        continue;
+    if (symbol == 'q' or symbol == 'e'):
+        continue
     print(symbol, end="")
