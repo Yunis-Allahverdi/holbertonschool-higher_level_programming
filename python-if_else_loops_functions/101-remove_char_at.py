6caa02519608ff99f62b31fc1dd4ebe23a6ce559
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-11-30 19:18:19 +0000

    Fixed finally

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    num = len(str)
-    first_part = str[:n]
-    last_part = str[n+1:num-1]
-    total = first_part + last_part
-    print(total, end="")
+    return str[:n] + str[n+1:]
