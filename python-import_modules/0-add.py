8518c37c51be054682b74631f345d93532c23e0c
root <allahverdiyunis@gmail.com>
2025-12-01 13:09:45 +0000

    Fixed finally

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as func
+from add_0 import add
 a = 1
 b = 2
 if __name__ == "__main__":
-    result = func.add(a, b)
+    result = add(a, b)
     print("The result is {}".format(result))
