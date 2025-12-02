cabd9c066990c6d9d7b3cbe3c3d061ecf2c3e544
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 14:39:17 +0000

    First

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    for x, y in a_dictionary.items():
+        new_dict[x] = y * 2
+    return a_dictionary
+    return new_dict
+
+
+a_dictionary = {}
