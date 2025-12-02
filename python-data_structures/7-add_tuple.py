29606ac22821cb26672c46a4c77b6f80a604780c
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 10:38:37 +0000

    Initial

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        list(tuple_a).append(0, 0)
+        a, b = tuple_a
+        a1, b1 = tuple_b
+    elif len(tuple_b) == 1:
+        list(tuple_a).append(0)
+        a, b = tuple_a
+        a1, b1 = tuple_b
+    else:
+        a, b, *c = tuple_a
+        a1, b1, *c1 = tuple_b
+    new_tuple = ((a + b), (a1 + b1))
+    return new_tuple
+
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
