2ea34dd1b7300d8bd0bcbcb94525eedaf18bbf67
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 14:58:25 +0000

    Fixed

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    new_list = list(sorted(a_dictionary))
+    new_list = list(a_dictionary)
     best_key = new_list[0]
     if len(a_dictionary) == 0:
         best_key = None
