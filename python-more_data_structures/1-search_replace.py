e4d2c8d602624eb34527173a1614a8791db3bb6d
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 13:25:42 +0000

    First

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list[search] = replace
+    for i in my_list:
+        if i == search:
+            my_list[i] = replace
     return my_list
 
 
