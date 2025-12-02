7537a12282edeb565e6fc05f7f8c42301327489a
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 09:24:30 +0000

    Fixed

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
             print("{:d}".format(row[i], end=" "))
-
-        print("{}".format("\n"))
+        
+        print()
 
 
 matrix = [[]]
