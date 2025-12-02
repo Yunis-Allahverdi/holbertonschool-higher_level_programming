0afc34312bf68ed00407b31fe3847e2f1555c977
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 14:45:21 +0000

    First

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
+    new_dict = {}
     for x, y in a_dictionary.items():
-        new_dict[x] = int(y) * 2
-    return a_dictionary
+        new_dict[x] = y * 2
     return new_dict
 
 
