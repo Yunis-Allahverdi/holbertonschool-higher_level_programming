423e794e3bb8461c465b5c17045832d5331898b0
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 18:25:57 +0000

    Fixed finally

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or len(a_dictionary) == None:
+    if len(a_dictionary) == 0 or a_dictionary is None:
         return None
-        exit()
-    else:
-     best_key = None
-     max_val = 0
-     for key, value in a_dictionary.items():
+
+    best_key = None
+    max_val = 0
+    for key, value in a_dictionary.items():
         if value > max_val:
             max_val = value
             best_key = key
 
     print("Best: {}".format(best_key))
     exit()
+
 
 a_dictionary = {}
