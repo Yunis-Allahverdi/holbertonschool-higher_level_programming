e5e366065cf6a4d39c7543bd488f02adce90288d
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 11:05:49 +0000

    Fixed

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        list_a = list(tuple_a)
-        list_a.append([0, 0])
-        tuple_a = list(list_a)
+    if len(tuple_a) == 2 and len(tuple_b) == 2:
         a, b = tuple_a
         a1, b1 = tuple_b
-    elif len(tuple_b) == 1:
-        list_a = list(tuple_a)
-        list_a.append(0)
-        tuple_a = list(list_a)
-        a, b = tuple_a
-        a1, b1 = tuple_b
-    else:
+    elif len(tuple_a) > 2 and len(tuple_b) > 2:
         a, b, *c = tuple_a
         a1, b1, *c1 = tuple_b
+    elif len(tuple_a) > 2 and len(tuple_b) == 2:
+        a, b, *c = tuple_a
+        a1, b1 = tuple_b
+    elif len(tuple_a) == 2 and len(tuple_b) > 2:
+        a, b = tuple_a
+        a1, b1, *c1 = tuple_b
+    else:
+        list_a = list(tuple_a)
+        list_b = list(tuple_b)
+        for i in range(list_a):
+            if len(list_a) < 2:
+                list_a.append(0)
+
+        for i in range(list_b):
+            if len(list_b) < 2:
+                list_b.append(0)
+        tuple_a = tuple(list_a)
+        tuple_b = tuple(list_b)
+        a, b = tuple_a
+        a1, b1 = tuple_b
+
     new_tuple = ((a + a1), (b + b1))
     return new_tuple
 
