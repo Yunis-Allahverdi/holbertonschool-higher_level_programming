7f9838593ec91b1f5b74cdd76633bfd5f5c3cd22
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 14:09:29 +0000

    First

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary):
-        print(i)
+    for x, y in sorted(a_dictionary).items():
+        print(x, ":", y)
 
 
 a_dictionary = {}
