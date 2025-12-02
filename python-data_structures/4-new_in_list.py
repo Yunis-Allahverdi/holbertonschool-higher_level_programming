d2ccfdc5a6e8054174ed240dbb155330a0062738
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 08:35:45 +0000

    Initial

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (idx < 0) or (idx >= len(my_list)):
+        copy_list = my_list.copy()
+        return copy_list
+    else:
+        new_list = my_list.cop()
+        new_list[idx] = element
+        return new_list
+        exit()
+
+
+my_list = []
+idx = 0
+element = 0
