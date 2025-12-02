58ab33ff34b31d350e3868522101fa7da018895a
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 08:43:19 +0000

    Fixed

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in my_string:
-        if (i != 'c') or (i != 'C'):
+        if (i != 'c') and (i != 'C'):
             print(i, end="")
         else:
             continue
