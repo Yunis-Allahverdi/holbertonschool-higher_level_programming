26749e4a4bd4e6eebc1083e9e9d3e3df1c5779b1
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 19:17:20 +0000

    First

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    int_list = []
+    if roman_string is not str or roman_string is None:
+        return 0
+    else:
+        for i in roman_string:
+            if i == "X":
+                int_list.append(10)
+            elif i == "I":
+                int_list.append(1)
+            elif i == "V":
+                int_list.append(5)
+            elif i == "L":
+                int_list.append(50)
+            elif i == "C":
+                int_list.append(100)
+            elif i == "D":
+                int_list.append(500)
+            else:
+                return 0
+        result = 0
+        for i in range(len(int_list)):
+            if i == len(int_list) - 1:
+                result += i
+                break
+            else:
+                if int_list[i] >= int_list[i+1]:
+                    result += i
+                else:
+                    result -= i
+
+        return result
