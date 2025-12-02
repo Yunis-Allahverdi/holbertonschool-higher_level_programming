7d702b5aeb043de7e4aa2481ee316d83504d18e7
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 18:20:19 +0000

    Fixed finally

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not in a_dictionary:
+    if len(a_dictionary) == 0 or len(a_dictionary) == None:
         return None
         exit()
     else:
