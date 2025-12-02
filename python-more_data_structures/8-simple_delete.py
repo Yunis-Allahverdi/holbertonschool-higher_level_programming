ee017500b8d6774a4cfd119a3de88045e637298a
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-02 14:30:20 +0000

    First

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     new_dict = a_dictionary
-    if key in new_dict:   
+    if key in new_dict: 
         del new_dict[key]
     return a_dictionary
     return new_dict
