dc0fc3379ed109869599bce3aee7c6492826a553
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-03 05:51:32 +0000

    First

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    index = 1
+    for i in my_list:
+        index += 1
+
+    try:
+        if x > index:
+            raise IndexError("Try again")
+        else:
+            for i in range(x):
+                print(my_list[i], end="")
+    except IndexError as i:
+        print(i)
+
+
+my_list = []
