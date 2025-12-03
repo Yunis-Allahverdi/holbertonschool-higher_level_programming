2214ebf5381bb4676f4d56f79b2f1f6b113f59f3
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-03 05:58:17 +0000

    Fixed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,20 +1,10 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    index = 1
-    for i in my_list:
-        index += 1
-
-    try:
-        if x > index:
-            raise IndexError("Try again")
-        else:
-            for i in range(x):
-                print(my_list[i], end="")
-
-            np_print = int(x)
-            print("{:d}".format(np_print))
-    except IndexError as i:
-        print(i)
-
-
-my_list = []
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
