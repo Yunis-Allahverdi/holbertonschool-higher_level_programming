1280badf65b46c23a690eda678037388b09f9758
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-03 06:20:06 +0000

    First

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{}".format(int(value)))
+        print("{:d}".format(int(value)))
     except TypeError:
         print("{} is not an integer".format(value))
