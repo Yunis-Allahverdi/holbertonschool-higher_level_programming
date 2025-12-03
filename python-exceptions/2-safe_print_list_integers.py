713de235cdc20fb7a3687a8dbe5c67c0403ced75
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-03 08:25:53 +0000

    First

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,14 +6,14 @@ def safe_print_list_integers(my_list=[], x=0):
             if not isinstance(i, int):
                 continue
             else:
-                print("{:d}".format(my_list[i]), end="")
+                print("{:d}".format(my_list[count]), end="")
             count += 1
         except IndexError:
             raise
             break
 
     print()
-    exit()
+    return count
 
 
 my_list = []
