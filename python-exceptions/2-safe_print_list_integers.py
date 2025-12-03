c26a60b3ea11876c4adfd61ef01fc0c35e21d30d
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-03 08:24:15 +0000

    First

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in my_list:
+        try:
+            if not isinstance(i, int):
+                continue
+            else:
+                print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            raise
+            break
+
+    print()
+    exit()
+
+
+my_list = []
