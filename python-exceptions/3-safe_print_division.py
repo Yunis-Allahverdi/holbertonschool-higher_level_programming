5d1fbc6c75d44aea2233c627c8ccba5949bd16ec
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-03 09:06:25 +0000

    First

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError as err:
+        result = None
+    finally:
+        return "Inside result: {}".format(result)
