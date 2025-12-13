cf309912faa9c5f1cb91e49f71e795c0b3337a4a
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-13 15:20:42 +0000

    Fixed

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module provides function and class
+for editing lists
+'''
+
+
+class MyList(list):
+    '''
+    A class that inherits from list
+    '''
+
+    def print_sorted(self):
+        return list.sort()
