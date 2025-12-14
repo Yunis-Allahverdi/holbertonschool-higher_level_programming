7d12aa36302cd2274ad3fb3c6fb4c9f21e0a2426
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-14 10:48:49 +0000

    Fixed

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,6 +6,10 @@ class is inherited or not
 
 
 def is_kind_of_class(obj, a_class):
+    '''
+    Function that checks obj
+    is or not inherited from class
+    '''
     if isinstance(obj, a_class):
         return True
     return False
