a1be419784a770c608032278b5b4ac8456233068
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-14 14:59:27 +0000

    Fixed

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -13,6 +13,7 @@ def load_from_json_file(filename):
     Save to json file function
     '''
 
-    with open(filename, 'r', encoding='UTF8') as f:
+    with open(filename, 'r') as f:
         obj = f.read()
         my_list = json.loads(obj)
+        return my_list
