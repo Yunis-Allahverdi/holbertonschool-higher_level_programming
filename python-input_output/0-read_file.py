d0aec43592f831d75bf0de6f775fe6cfa72f6e8c
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-14 14:32:30 +0000

    Fixed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,10 @@ Module that reads text file
 
 
 def read_file(filename=""):
+    '''
+    Function that reads file
+    '''
+
     with open('UTF8') as f:
         for line in f:
             print(line, end="")
