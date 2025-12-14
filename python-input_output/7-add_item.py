6cc31eebe737c74879b0c1e04b935f7870878ae7
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-14 15:38:05 +0000

    Fixed

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -17,7 +17,7 @@ def save_to_json_file(my_obj, filename):
     Save to json file function
     '''
 
-    with open(filename, 'w') as f:
+    with open(filename, 'a') as f:
         f.write(json.dumps(my_obj))
 
 
