15cc7c1d31f4af1cc5ad51d57e486f78fb2a6ba4
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-14 09:55:50 +0000

    Fixed

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Module that includes func which returns True
+if obj is instance of class
+'''
+
+
+def is_same_class(obj, a_class):
+    '''
+    Function that checks whether
+    obj is instance of a_class or not
+    '''
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
