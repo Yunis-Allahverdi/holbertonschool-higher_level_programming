2dde5cc118375937258b21793b5cd82ff803c92b
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-14 16:44:35 +0000

    Fixed

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+Module that writes a Student Class
+'''
+
+
+class Student:
+    '''
+    A student class
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            for i in attrs:
+                return self.i
+            return attrs
