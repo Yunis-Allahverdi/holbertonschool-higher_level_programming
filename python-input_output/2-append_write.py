275e58f164aa09c7cc1d154dcb2189d9cc81013e
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-14 14:38:16 +0000

    Fixed

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Module that reads text file
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Function that reads file
+    '''
+
+    with open(filename, 'a', encoding='UTF8') as f:
+        return f.write(text)
