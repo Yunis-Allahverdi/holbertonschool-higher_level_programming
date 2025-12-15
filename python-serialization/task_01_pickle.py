03579226f8ecaee7cb94f317e2f0233d7b1fa133
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-15 11:46:14 +0000

    Fixed

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+Module that serialize and deserialize data
+using pickle module
+'''
+
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+        self.attr_dict = {"Name": name, "Age": age, "Is Student": is_student}
+
+    def serialize(self, filename):
+        with open(filename, 'wb') as w_file:
+            pickle.dump(self.attr_dict, w_file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'rb') as r_file:
+            return cls(pickle.load(r_file))
+
+    def display(self):
+        for key, value in self.attr_dict.items():
+            return "{}: {}".format(key, value)
