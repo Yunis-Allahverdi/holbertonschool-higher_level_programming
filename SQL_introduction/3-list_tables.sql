911127f293e13d7f1e1c4ec4c7a8fc628cb7e1af
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-16 14:56:29 +0000

    Fixed

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,4 @@
 -- Show 3 tables based on given database mysql
 
-SELECT * FROM information_schema.tables 
-WHERE table_schema = 'mysql' LIMIT 3;
+SELECT * FROM information_schema.tables WHERE table_schema = 'mysql' LIMIT 3;
 
