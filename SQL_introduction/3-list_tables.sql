cb18555f41c467d908ae07c351bced6fa3d2dc3d
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-16 15:03:51 +0000

    Fixed

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,3 @@
 -- Show 3 tables based on given database mysql
-
-SELECT * FROM information_schema.tables WHERE table_schema = 'mysql' and table_type='BASE TABLE' LIMIT 3;
+SELECT table_name FROM information_schema.tables WHERE table_schema = 'mysql' and table_type='BASE TABLE' LIMIT 3;
 
