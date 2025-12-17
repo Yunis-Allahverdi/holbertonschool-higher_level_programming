e2f291448860e11afb513d64213b3821a73e25d8
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-17 09:52:44 +0000

    Fixed

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,8 +1,8 @@
 -- Creating table
 
-USE hbtn_0c_0;
+--USE hbtn_0c_0;
 
 CREATE TABLE IF NOT EXISTS first_table (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
+	id INT,
+	name VARCHAR(256)
 );
