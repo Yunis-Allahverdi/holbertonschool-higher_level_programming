d485f87fe4b18f260b6452f3f61feacac9b2420d
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-17 12:26:09 +0000

    Fixed

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- number of records with the same record
 
-SELECT score, COUNT(score) AS number
-FROM second_table DESC
+SELECT score, COUNT(score) DESC AS number
+FROM second_table
 GROUP BY score;
