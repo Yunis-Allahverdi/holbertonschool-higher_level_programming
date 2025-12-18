1e5b2c405648aab0f285742cd9e9454e9209c592
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-18 18:03:24 +0000

    Fixed

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- List all cities of California
 
 USE hbtn_0d_usa;
-SELECT id, name FROM cities NATURAL JOIN states WHERE name = 'California' ORDER BY cities.id
+SELECT id, name FROM cities WHERE name = 'California' ORDER BY cities.id
