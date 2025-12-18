588e0756b5b43ccaaccce96261549fa07db1f1af
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-18 14:39:51 +0000

    Fixed

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,7 @@
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
