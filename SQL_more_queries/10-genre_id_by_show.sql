8706066b26ebf0c0fa910dcb6c9a3dea4dd6f5bc
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-19 10:36:00 +0000

    Fixed

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -6,5 +6,5 @@ USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ON tv_show_genres.show_id = tv_shows.id;
+-- ORDER BY tv_shows.title, tv_show_genres.genre_id;
