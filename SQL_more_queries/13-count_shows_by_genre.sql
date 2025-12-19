fc2d43db44a92ea93df1565123ed914ebcf67b08
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-19 12:13:25 +0000

    Fixed

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List all genres and num of shows linked to each
 
-SELECT tv_show_genres.`genre_id`, COUNT(DISTINCT tv_shows.id) AS number_of_shows
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id;
+SELECT tv_show_genres.`genre_id`, COUNT(DISTINCT `tv_shows.id`) AS number_of_shows
+FROM `tv_shows`
+INNER JOIN `tv_show_genres`
+ON tv_shows.`id` = tv_show_genres.`show_id`;
