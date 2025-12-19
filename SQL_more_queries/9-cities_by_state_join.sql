ccf26406b18880ccd717f0b1f7c33ba52eb071e9
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-19 05:14:26 +0000

    Fixed

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- script that lists all cities --
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states
+ON cities.id = states.id
+ORDER BY cities.id;
