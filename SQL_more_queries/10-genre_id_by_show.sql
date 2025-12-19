2ca2c3b46d16a74f91e24bdd7e54642179c49eb8
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-19 11:11:08 +0000

    Fixed

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
 
 USE hbtn_0d_tvshows;
 
--- SELECT tv_shows.title, tv_show_genres.genre_id
---FROM tv_shows
---INNER JOIN tv_show_genres
---ON tv_show_genres.show_id = tv_shows.id;
--- ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.id = tv_shows.show_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
