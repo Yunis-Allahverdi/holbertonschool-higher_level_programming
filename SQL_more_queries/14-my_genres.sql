e505656f3e45b1cd759d2b07fb5561aaf4baa939
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-19 12:36:09 +0000

    Fixed

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,3 @@
 -- List all genres of the show DEXTER
 
-SELECT DISTINCT tv_genres.name AS name FROM tv_genres JOIN tv_shows WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
+SELECT tv_genres.name AS name FROM tv_genres JOIN tv_shows WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
