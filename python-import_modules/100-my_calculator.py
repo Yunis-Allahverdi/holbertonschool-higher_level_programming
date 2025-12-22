c5885e8a3204469c4009b5c71b22cee219ec3b02
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-22 14:34:27 +0000

    Fixed

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         if sys.argv[2] == '+':
-            print(f"{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], add(sys.argv[1],sys.argv[3])))
+            print("{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], add(sys.argv[1],sys.argv[3])))
         if sys.argv[2] == '-':
-            print(f"{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], sub(sys.argv[1],sys.argv[3])))
+            print("{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], sub(sys.argv[1],sys.argv[3])))
         if sys.argv[2] == '*':
-            print(f"{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], mul(sys.argv[1],sys.argv[3])))
+            print("{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], mul(sys.argv[1],sys.argv[3])))
         if sys.argv[2] == '/':
-            print(f"{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], div(sys.argv[1],sys.argv[3])))
+            print("{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], div(sys.argv[1],sys.argv[3])))
             return 0
         print("Unknown operator. Available operators: +, -, * and /")
         return 1
