d3ddd5139f7d9ec39439fba7fb709eeb20e4f232
Yunis-Allahverdi <allahverdiyunis@gmail.com>
2025-12-22 14:43:03 +0000

    Fixed

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,16 +4,18 @@ from calculator_1.py import add, sub, div, mul
 from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    if len(sys.argv) == 3:
         if sys.argv[2] == '+':
             print("{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], add(sys.argv[1],sys.argv[3])))
-        if sys.argv[2] == '-':
+        elif sys.argv[2] == '-':
             print("{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], sub(sys.argv[1],sys.argv[3])))
-        if sys.argv[2] == '*':
+        elif sys.argv[2] == '*':
             print("{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], mul(sys.argv[1],sys.argv[3])))
-        if sys.argv[2] == '/':
+        elif sys.argv[2] == '/':
             print("{} {} {} = {} ".format(sys.argv[1], sys.argv[2], sys.argv[3], div(sys.argv[1],sys.argv[3])))
-            return 0
-        print("Unknown operator. Available operators: +, -, * and /")
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            return 1
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         return 1
